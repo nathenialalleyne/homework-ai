76e581480dea9cb1833c31a883c080852fee9109
nateachino <natealleyne88@gmail.com>
2023-10-03 12:29:00 -0400

    Changed landing page depending on user status

--- src/pages/landing/header.tsx
+++ src/pages/landing/header.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Button from '../components/button'
-import Image from 'next/image'
+import { SignUpButton, SignInButton, SignedIn, SignedOut } from "@clerk/nextjs";
 
 export default function LandingHeader() {
     return (
@@ -9,8 +9,21 @@ export default function LandingHeader() {
             <div className='pr-4 flex gap-4'>
                 <Button text='About' link='/pricing' style='bg-transparent p-2  text-white w-fit font-semibold transition-all ease-in-out hover:scale-110 hover:text-slate-200' />
                 <Button text='Pricing' link='/pricing' style='bg-transparent p-2  text-white w-fit font-semibold transition-all ease-in-out hover:scale-110 hover:text-slate-200' />
-                <Button text='Sign in' style='bg-transparent p-2 text-white w-fit font-semibold transition-all ease-in-out hover:scale-110 hover:text-slate-200' />
-                <Button text='Sign up' style='bg-gradient-to-r from-yellow-200 via-yellow-400 to-rose-400 p-2 pl-4 pr-4 text-black w-fit font-semibold transition-all ease-in-out hover:scale-110' />
+
+
+                <SignedOut>
+                    <SignInButton>
+                        <Button text='Sign in' style='bg-transparent p-2 text-white w-fit font-semibold transition-all ease-in-out hover:scale-110 hover:text-slate-200' />
+                    </SignInButton>
+
+                    <SignUpButton>
+                        <Button text='Sign up' style='bg-gradient-to-r from-yellow-200 via-yellow-400 to-rose-400 p-2 pl-4 pr-4 text-black w-fit font-semibold transition-all ease-in-out hover:scale-110' />
+                    </SignUpButton>
+                </SignedOut>
+
+                <SignedIn>
+                    <Button text='Profile' link='/dashboard' style='bg-white p-2 pl-4 pr-4 text-black w-fit font-semibold transition-all ease-in-out hover:scale-110' />
+                </SignedIn>
             </div>
         </div>
     )
