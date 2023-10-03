f0dc089572b1aa917e8431d7e33398aedbb63aa9
nateachino <natealleyne88@gmail.com>
2023-10-03 11:59:01 -0400

    added plans and pricing heading

--- src/pages/pricing/index.tsx
+++ src/pages/pricing/index.tsx
@@ -10,6 +10,7 @@ export default function Pricing() {
             <LandingHeader />
                 <div className='absolute z-30 w-screen h-screen flex flex-col justify-center items-center gap-6'>
                 <AboutParagraph />
+                <h2 className='text-white text-5xl font-bold'>Plans and Pricing</h2>
                 <div className='w-screen flex justify-center items-center gap-4'>
                         <PriceBox price='0' plan='Free' features={['Sample and mimic your writing style', '5 Assignments Monthly', '1 Source per Assignment', 'Limited Revisions']} />
                         <PriceBox price='15' plan='Premium' features={['Sample and mimic your writing style', 'Unlimited Monthly Assignments', 'Unlimited Sources per Assignment', 'Unlimited Revisions', 'AI-Powered Note Taking']} />
