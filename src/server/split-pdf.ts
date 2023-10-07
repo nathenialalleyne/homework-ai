2147bc301d388b8ed75cf88ef0427076044b7555
nateachino <natealleyne88@gmail.com>
2023-10-07 19:10:01 -0400

    made split algo run concurrently

--- src/server/split-pdf.ts
+++ src/server/split-pdf.ts
@@ -1,17 +1,17 @@
-import {PDFDocument} from 'pdf-lib';
+import { PDFDocument } from 'pdf-lib';
 import fs from 'fs';
-import {uploadSplitFile} from './upload-file';
+import { uploadSplitFile } from './upload-file';
 
 export default async function splitPDF(inputPDFPath: string) {
-    const files = []
     const pagesPerSection = 5;
     const randomID = Math.random().toString(36).substring(7);
     try {
         const inputPDFBuffer = await fs.promises.readFile(inputPDFPath);
         const pdfDoc = await PDFDocument.load(inputPDFBuffer);
         const totalPages = pdfDoc.getPageCount();
-
         const numSections = Math.ceil(totalPages / pagesPerSection);
+
+        const sectionPromises = [];
 
         for (let sectionIndex = 0; sectionIndex < numSections; sectionIndex++) {
             const startPage = sectionIndex * pagesPerSection + 1;
@@ -23,12 +23,17 @@ export default async function splitPDF(inputPDFPath: string) {
                 const [copiedPage] = await sectionPDF.copyPages(pdfDoc, [pageIndex]);
                 sectionPDF.addPage(copiedPage);
             }
+
             const sectionPDFBytes = await sectionPDF.save();
 
-            const upload = await uploadSplitFile(Buffer.from(sectionPDFBytes), `${randomID}_${sectionIndex}.pdf`)
-            files.push(upload)
+            const sectionFileName = `${randomID}_${sectionIndex}.pdf`;
+            sectionPromises.push(uploadSplitFile(Buffer.from(sectionPDFBytes), sectionFileName));
         }
-        return files
+
+        // Wait for all section uploads to complete before returning
+        const files = await Promise.all(sectionPromises);
+
+        return files;
     } catch (error) {
         console.error('Error splitting PDF:', error);
     }
