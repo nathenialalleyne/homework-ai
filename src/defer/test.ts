ac9a53030cbdf8586739c108d87f134341c22d72
nateachino <natealleyne88@gmail.com>
2023-12-03 08:50:53 -0500

    test

--- src/defer/test.ts
+++ src/defer/test.ts
@@ -4,25 +4,12 @@ import { GoogleAuth } from 'google-auth-library'
 import { Storage } from '@google-cloud/storage'
 
 async function testGCP(){
-    
-        const GoogleAuthOptions = () => {   
-            return new GoogleAuth({
-                keyFile: 'gcp-key.json',
-                scopes: [
-                    'https://www.googleapis.com/auth/cloud-platform', 
-                    'https://www.googleapis.com/auth/cloud-vision',
-                    'https://www.googleapis.com/auth/devstorage.full_control',],
-            })
-        }
-    
-        const client = await GoogleAuthOptions().getClient()
-        const storage = new Storage({projectId: 'altrai', authClient: client})
-
-        const bucket = storage.bucket('pdf-source-storage-bucket')
-        const file = await bucket.file('test.txt').download()
-
-        return console.log(file.toString())
-
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      console.log(`Hello ${name}!`);
+      resolve("done");
+    }, 5000);
+  });
 }
 
 export default defer(testGCP)
