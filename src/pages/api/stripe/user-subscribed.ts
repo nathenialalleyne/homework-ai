fb0e0fd5ff07b5460b69c9ff7debc8b38db309f0
nateachino <natealleyne88@gmail.com>
2023-12-07 15:30:08 -0500

    test

--- src/pages/api/stripe/user-subscribed.ts
+++ src/pages/api/stripe/user-subscribed.ts
@@ -1,30 +1,30 @@
-import {NextApiRequest, NextApiResponse} from 'next';
-import { databaseRouter } from '@/server/api/routers/database-operations';
-import { db } from '@/server/db';
-import { getAuth } from '@clerk/nextjs/dist/types/server-helpers.server';
-import stripe from '@/utils/stripe';
+// import {NextApiRequest, NextApiResponse} from 'next';
+// import { databaseRouter } from '@/server/api/routers/database-operations';
+// import { db } from '@/server/db';
+// import { getAuth } from '@clerk/nextjs/dist/types/server-helpers.server';
+// import stripe from '@/utils/stripe';
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  let event
+// export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+//   let event
 
-  try{
-    event = stripe.webhooks.constructEvent(req.body, req.headers['stripe-signature'] as string[], process.env.STRIPE_TEST_KEY!)
-    switch(event.type) {
-      case 'payment_intent.succeeded':
-        const paymentIntent = event.data.object
-        const dbRouter = databaseRouter.createCaller({db: db, auth: getAuth(req)})
-        const userActions = await dbRouter.getRemainingUserActions({id: paymentIntent.metadata.userId!})
-        if(userActions === 0){
-          await dbRouter.updateUser({id: paymentIntent.metadata.userId!, actionsRemaining: 5})
-        }
-        else{
-          await dbRouter.updateUser({id: paymentIntent.metadata.userId!, actionsRemaining: userActions! + 5})
-        }
+//   try{
+//     event = stripe.webhooks.constructEvent(req.body, req.headers['stripe-signature'] as string[], process.env.STRIPE_TEST_KEY!)
+//     switch(event.type) {
+//       case 'payment_intent.succeeded':
+//         const paymentIntent = event.data.object
+//         const dbRouter = databaseRouter.createCaller({db: db, auth: getAuth(req)})
+//         const userActions = await dbRouter.getRemainingUserActions({id: paymentIntent.metadata.userId!})
+//         if(userActions === 0){
+//           await dbRouter.updateUser({id: paymentIntent.metadata.userId!, actionsRemaining: 5})
+//         }
+//         else{
+//           await dbRouter.updateUser({id: paymentIntent.metadata.userId!, actionsRemaining: userActions! + 5})
+//         }
 
-    }
-    res.status(200)
-  }
-  catch(e: any){
-    res.status(400).send(`Webhook Error: ${e.message}`)
-  }
-}
+//     }
+//     res.status(200)
+//   }
+//   catch(e: any){
+//     res.status(400).send(`Webhook Error: ${e.message}`)
+//   }
+// }
