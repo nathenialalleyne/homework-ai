6a04ada0944a8ac0e100ac60077d6e5658b96e61
nateachino <natealleyne88@gmail.com>
2023-12-13 02:18:55 -0500

    added stripe webhook endpoint

--- src/pages/api/stripe/user-subscribed.ts
+++ src/pages/api/stripe/user-subscribed.ts
@@ -1,30 +1,51 @@
-// import {NextApiRequest, NextApiResponse} from 'next';
-// import { databaseRouter } from '@/server/api/routers/database-operations';
-// import { db } from '@/server/db';
-// import { getAuth } from '@clerk/nextjs/dist/types/server-helpers.server';
-// import stripe from '@/utils/stripe';
+import stripe from '@/utils/stripe'
+import { NextApiRequest, NextApiResponse } from 'next'
 
-// export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-//   let event
+import { buffer } from 'micro'
 
-//   try{
-//     event = stripe.webhooks.constructEvent(req.body, req.headers['stripe-signature'] as string[], process.env.STRIPE_TEST_KEY!)
-//     switch(event.type) {
-//       case 'payment_intent.succeeded':
-//         const paymentIntent = event.data.object
-//         const dbRouter = databaseRouter.createCaller({db: db, auth: getAuth(req)})
-//         const userActions = await dbRouter.getRemainingUserActions({id: paymentIntent.metadata.userId!})
-//         if(userActions === 0){
-//           await dbRouter.updateUser({id: paymentIntent.metadata.userId!, actionsRemaining: 5})
-//         }
-//         else{
-//           await dbRouter.updateUser({id: paymentIntent.metadata.userId!, actionsRemaining: userActions! + 5})
-//         }
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+}
 
-//     }
-//     res.status(200)
-//   }
-//   catch(e: any){
-//     res.status(400).send(`Webhook Error: ${e.message}`)
-//   }
-// }
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  try {
+    const signature = req.headers['stripe-signature']
+    if (!signature) throw new Error('Stripe signature is required')
+
+    const buf = await buffer(req)
+    const endpointSecret =
+      'whsec_bb513486ef44d95ffb90c9104140efe8ffda649645d21c2168e350b6cbc4cba2'
+
+    let event: any
+
+    try {
+      event = stripe.webhooks.constructEvent(buf, signature, endpointSecret)
+      if (!event) console.log('Event is null')
+    } catch (err: any) {
+      console.log(err)
+      res.status(402).send(`Webhook Error: ${err.message}`)
+      return
+    }
+
+    switch (event.type) {
+      case 'payment_intent.succeeded':
+        const paymentIntentSucceeded = event.data.object
+        // Then define and call a function to handle the event payment_intent.succeeded
+        console.log(paymentIntentSucceeded)
+        break
+      // ... handle other event types
+      default:
+        console.log(`Unhandled event type ${event.type}`)
+    }
+
+    res.status(200).json({ received: true })
+  } catch (error: any) {
+    console.log(error)
+    return res.status(500).json({ error: error.message })
+  }
+}
